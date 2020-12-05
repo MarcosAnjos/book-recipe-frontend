b200c1722b5ad9a5b60f2c8aece6bb9a881dedce
Marcos Alexandre <marcosanjos@alunos.utfpr.edu.br>
2020-12-05 10:59:21 -0300

    axios confi.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://54.94.52.236:3333/',
-
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'foobar' }
 })
 
 export default api
